f7d3eb893a71716d39a3ac0d5340befc92de4a7e
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-04-30 14:11:23 +0200

    Started with Controller, added migration.Update readme.md

---
+++ Source/WebApplication1/Models/Parkings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Parkings
+    {
+        public int Id { get; set; }
+        public int Fee { get; set; }
+        public decimal MaxLength { get; set; }
+        public bool Occupied { get; set; }
+        [MaxLength(50)] public string ParkedBy { get; set; }
+        [MaxLength(50)] public string ShipName { get; set; }
+    }
+}
---
+++ Source/WebApplication1/Models/Payment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Payment
+    {
+
+        public int Id { get; set; }
+
+        [ForeignKey("SpacePort")]
+        public int SpacePortId { get; set; }
+
+        public string PersonName { get; set; }
+
+        public string SpaceShip { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+
+        public DateTime? EndTime { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool Payed { get; set; }
+    }
+}
