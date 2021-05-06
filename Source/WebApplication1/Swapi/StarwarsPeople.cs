dc2d32525cf37d58caea6ae6ecef1637255c8184
Mikael95 <pk.mikael@outlook.com>
2021-05-06 18:33:02 +0200

    changed method to get swapi data

--- Source/WebApplication1/Swapi/StarwarsPeople.cs
+++ Source/WebApplication1/Swapi/StarwarsPeople.cs
@@ -9,111 +9,26 @@ namespace WebApplication1.Swapi
 {
     public class StarwarsPeople
     {
-        private static async Task<PeopleResponse> GetPersonPage(int page) // Get one page from API
+        private const string baseURL = "http://swapi.dev/api/";
+
+        //Fetch people from API
+        public static async Task<List<Person>> GetPeople()
         {
-            var client = new RestClient("https://swapi.dev/api/");
-            var request = new RestRequest($"people/?page={page}", DataFormat.Json);
-            var response = await client.GetAsync<PeopleResponse>(request);
-            return response;
-        }
-        private static Task<PeopleResponse> CreateTask(int i) // Create task to run several requests async
-        {
-            return Task.Run(() => GetPersonPage(i));
-        }
-        public async Task<List<Person>> GetAllPersons() // Return list of all persons in API
-        {
-            var p = await GetPersonPage(1);
-            int numberOfPersons = p.Results.Count;
-            int personsPerOnePage = p.Results.Count;
-            int numberOfPages = (int)Math.Ceiling(1.0 * numberOfPersons / personsPerOnePage); // Use the number of persons and persons per page to calculate number of pages
+            var client = new RestClient(baseURL);
+            string requestUrl = $"http://swapi.dev/api/people/";
+            PeopleResponse response;
+            List<Person> persons = new List<Person>();
 
-            var tasks = new List<Task<PeopleResponse>>();
-            for (int i = 1; i < numberOfPages + 1; i++) // For each page create a task to run all pages at the same time.
+            while (requestUrl != null)
             {
-                tasks.Add(CreateTask(i));
-            }
+                string resource = requestUrl.Substring(baseURL.Length);
+                var request = new RestRequest(resource, DataFormat.Json);
+                response = await client.GetAsync<PeopleResponse>(request);
 
-            List<PeopleResponse> temp = new();
-            List<Person> results = new();
-
-            for (int i = 0; i < numberOfPages; i++)
-            {
-                temp.AddRange((IEnumerable<PeopleResponse>)tasks[i].Result.Results); // Add each page result to list
+                persons.AddRange(response.Results);
+                requestUrl = response.Next;
             }
-            foreach(var people in temp)
-            {
-                results.AddRange(people.Results);
-            }
-            return results;
+            return persons;
         }
-        /* public List<string> PeopleList { get; set; }
-
-         public StarwarsPeople()
-         {
-             PeopleList = new List<string>();
-         }
-
-         // Download one page of names to PeopleList.
-         public async Task<PeopleResponse> GetPeopleFromPageAsync(int page)
-         {
-             var client = new RestClient(basicURL);
-             var request = new RestRequest($"people/?page={page}", DataFormat.Json);
-
-             var peopleResponse = await client.GetAsync<PeopleResponse>(request);
-
-             return peopleResponse;
-
-         }
-
-         // Get number of pages avalible from swapi.
-         public async Task<IEnumerable<int>> GetPagesAsync()
-         {
-             bool finnishedList = true;
-             IList<int> pages = new List<int>();
-             int page = 1;
-             var client = new RestClient(basicURL);
-             //var request = new RestRequest($"people/?page={page}", DataFormat.Json);
-             //var peopleResponse = await client.GetAsync<PeopleResponse>(request);
-
-             while (finnishedList)
-             {
-                 var request = new RestRequest($"people/?page={page}", DataFormat.Json);
-                 var peopleResponse = Task.Run(async () => await client.GetAsync<PeopleResponse>(request));
-
-                 if (peopleResponse.Result.Count != 0)
-                 {
-                     pages.Add(page);
-                     page++;
-                 }
-                 else
-                 {
-                     finnishedList = false;
-                 }
-             }
-             return pages;
-         }
-
-
-         // Add people to list async.
-         public async void DownloadPeopleAsync(IEnumerable<int> pages)
-         {
-             List<Task<PeopleResponse>> tasks = new List<Task<PeopleResponse>>();
-
-             foreach (var page in pages)
-             {
-                 Task<PeopleResponse> task = GetPeopleFromPageAsync(page);
-                 tasks.Add(task);
-             }
-
-             Task.WaitAll(tasks.ToArray());
-
-             foreach (var task in tasks)
-             {
-                 foreach (var p in task.Result.Results)
-                 {
-                     this.PeopleList.Add(p.Name);
-                 }
-             }
-         }*/
     }
 }
--- Source/WebApplication1/Swapi/StarwarsShips.cs
+++ Source/WebApplication1/Swapi/StarwarsShips.cs
@@ -9,112 +9,24 @@ namespace WebApplication1.Swapi
 {
     public class StarwarsShips
     {
-        private static async Task<StarshipResponse> GetShipPage(int page) // Get one page from API
+        private const string baseURL = "http://swapi.dev/api/";
+        public static async Task<List<Starship>> GetStarships()
         {
-            var client = new RestClient("https://swapi.dev/api/");
-            var request = new RestRequest($"people/?page={page}", DataFormat.Json);
-            var response = await client.GetAsync<StarshipResponse>(request);
-            return response;
-        }
-        private static Task<StarshipResponse> CreateTask(int i) // Create task to run several requests async
-        {
-            return Task.Run(() => GetShipPage(i));
-        }
-        public async Task<List<Starship>> GetAllShips() // Return list of all ships in API
-        {
-            var p = await GetShipPage(1);
-            int numberOfShips = p.Results.Count;
-            int shipsPerOnePage = p.Results.Count;
-            int numberOfPages = (int)Math.Ceiling(1.0 * numberOfShips / shipsPerOnePage); // Use the number of ships and ships per page to calculate number of pages
+            var client = new RestClient(baseURL);
+            string requestUrl = "http://swapi.dev/api/starships/";
+            StarshipResponse response;
+            List<Starship> starships = new List<Starship>();
 
-            var tasks = new List<Task<StarshipResponse>>();
-            for (int i = 1; i < numberOfPages + 1; i++) // For each page create a task to run all pages at the same time.
+            while (requestUrl != null)
             {
-                tasks.Add(CreateTask(i));
-            }
+                string resource = requestUrl.Substring(baseURL.Length);
+                var request = new RestRequest(resource, DataFormat.Json);
+                response = await client.GetAsync<StarshipResponse>(request);
 
-            List<StarshipResponse> temp = new();
-            List<Starship> results = new();
-
-            for (int i = 0; i < numberOfPages; i++)
-            {
-                temp.AddRange((IEnumerable<StarshipResponse>)tasks[i].Result.Results); // Add each page result to list
+                starships.AddRange(response.Results);
+                requestUrl = response.Next;
             }
-
-            foreach (var ships in temp)
-            {
-                results.AddRange(ships.Results);
-            }
-            return results;
+            return starships;
         }
-        /* public List<string> ShipListList { get; set; }
-
-         public StarwarsShips()
-         {
-             ShipListList = new List<string>();
-         }
-
-         // Download one page of names to PeopleList.
-         public async Task<StarshipResponse> GetShipsFromPageAsync(int page)
-         {
-             var client = new RestClient(basicURL);
-             var request = new RestRequest($"people/?page={page}", DataFormat.Json);
-
-             var peopleResponse = await client.GetAsync<StarshipResponse>(request);
-
-             return peopleResponse;
-
-         }
-
-         // Get number of pages avalible from swapi.
-         public async Task<IEnumerable<int>> GetPagesAsync()
-         {
-             bool finnishedList = true;
-             IList<int> pages = new List<int>();
-             int page = 1;
-             var client = new RestClient(basicURL);
-             //var request = new RestRequest($"people/?page={page}", DataFormat.Json);
-             //var peopleResponse = await client.GetAsync<PeopleResponse>(request);
-
-             while (finnishedList)
-             {
-                 var request = new RestRequest($"people/?page={page}", DataFormat.Json);
-                 var starshipResponse = Task.Run(async () => await client.GetAsync<StarshipResponse>(request));
-
-                 if (starshipResponse.Result.Count != 0)
-                 {
-                     pages.Add(page);
-                     page++;
-                 }
-                 else
-                 {
-                     finnishedList = false;
-                 }
-             }
-             return pages;
-         }
-
-
-         // Add people to list async.
-         public async void DownloadShipsAsync(IEnumerable<int> pages)
-         {
-             List<Task<StarshipResponse>> tasks = new List<Task<StarshipResponse>>();
-
-             foreach (var page in pages)
-             {
-                 Task<StarshipResponse> task = GetShipsFromPageAsync(page);
-                 tasks.Add(task);
-             }
-
-             Task.WaitAll(tasks.ToArray());
-
-             foreach (var task in tasks)
-             {
-                 foreach (var p in task.Result.Results)
-                 {
-                     this.ShipListList.Add(p.Name);
-                 }
-             }
-         }*/
     }
 }
